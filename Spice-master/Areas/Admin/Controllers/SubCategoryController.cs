5c17ab542045cff2a4021ffe8fd43345dd71324c
wessamju1 <81860378+wessamju1@users.noreply.github.com>
2021-04-20 14:27:04 +0200

    ..

---
+++ Spice-master/Areas/Admin/Controllers/SubCategoryController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spice.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class SubCategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public SubCategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        //Get Index 
+        public async Task<IActionResult> Index() 
+        {
+            //.include is to get the main category details from model
+            var SubCategories = await _db.SubCategory.Include(s=> s.Category).ToListAsync();
+            return View(SubCategories);
+        }
+    }
+}
---
+++ Spice-master/Models/SubCategory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Spice.Models
+{
+    public class SubCategory
+    {
+        [Key]
+        public int Id { get; set; }
+        // you can use this to change the display of a property in asp-for 
+        //[Display(Name="Category Name ")]
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [Display(Name = "Category")]
+        public int CategoryId { set; get; }
+    
+        [ForeignKey("CategoryId")]
+        public virtual Category Category { set; get; }
+    
+    
+    }
+}
